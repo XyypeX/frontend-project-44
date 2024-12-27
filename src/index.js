b0f1bb9c229c16b4771f343805a66373d79ffc38
XypeX <tarasov-valerochka@bk.ru>
2024-12-27 21:17:41 +0300

    started tracking src/index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+import { greet } from './cli.js';
+
+export const runGame = (gameRules, generateRound) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = greet(); 
+  console.log(gameRules);
+
+  const roundsCount = 3;
+
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [question, correctAnswer] = generateRound();
+    console.log('Question: ' + question);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== correctAnswer.toString()) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
