46be58ed322047e103cf4d19add37b39d67eccc2
XypeX <tarasov-valerochka@bk.ru>
2025-01-17 22:01:58 +0300

    modified:   games/gcd.js

---
+++ games/gcd.js
@@ -0,0 +1,23 @@
+import { runGame } from '../src/index.js';
+
+const getGCD = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return getGCD(b, a % b);
+};
+
+const generateRound = () => {
+  const num1 = Math.floor(Math.random() * 100) + 1; // числа от 1 до 100
+  const num2 = Math.floor(Math.random() * 100) + 1;
+  
+  const question = `${num1} ${num2}`;
+  const answer = getGCD(num1, num2);
+
+  // Возвращаем массив, потому что index.js ожидает именно такой формат
+  return [question, answer];
+};
+
+const gameRules = 'Find the greatest common divisor of given numbers.';
+
+export default () => runGame(gameRules, generateRound);
