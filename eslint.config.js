3f8162a682efd638a48731f724015a75a76beb56
XypeX <tarasov-valerochka@bk.ru>
2025-02-05 22:35:52 +0300

    modified:   eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,24 @@
-import globals from "globals";
-
+import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 'latest', // Используем последнюю версию ECMAScript
+      sourceType: 'module', // Указываем, что используем модули
+    },
+    rules: {
+      // Базовые правила
+      'indent': ['error', 2], // Отступы в 2 пробела
+      'quotes': ['error', 'single'], // Использование одинарных кавычек
+      'semi': ['error', 'always'], // Точка с запятой обязательна
+      'no-multi-spaces': 'error', // Запрет на множественные пробелы
+      'eol-last': ['error', 'always'], // Новая строка в конце файла
+      'no-trailing-spaces': 'error', // Запрет на пробелы в конце строк
+      'prefer-const': 'error', // Использование const вместо let,если переменная не переопределяется
+      'max-len': ['error', { 'code': 100 }], // Максимальная длина строки 100 символов
+    },
+    ignores: ['node_modules/', 'dist/'], // Игнорируемые директории
+  },
 ];
